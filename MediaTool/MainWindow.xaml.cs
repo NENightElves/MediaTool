19dcd47fd796f96898764cf30cd44069e418e5ba
NENightElves <NENightElves@qq.com>
2020-09-29 03:18:10 +0800

    init

---
+++ MediaTool/MainWindow.xaml.cs
@@ -0,0 +1,102 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MediaTool
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        ObservableCollection<MediaItem> lMediaItem;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Debug.WriteLine(cmb.Text);
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            lMediaItem = new ObservableCollection<MediaItem>();
+            listView.ItemsSource = lMediaItem;
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Multiselect = true;
+            ofd.Filter = "视频文件|*.mkv;*.avi;*.mp4";
+            if (ofd.ShowDialog() == true)
+            {
+                foreach (String fileName in ofd.FileNames)
+                {
+                    lMediaItem.Add(new MediaItem(fileName));
+                }
+            }
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            for (int i = listView.SelectedItems.Count - 1; i >= 0; i--)
+            {
+                lMediaItem.Remove((MediaItem)listView.SelectedItems[i]);
+            }
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            String s;
+            foreach (MediaItem item in lMediaItem)
+            {
+                item.Status = "等待中";
+            }
+            new Thread(() =>
+            {
+                foreach (MediaItem item in lMediaItem)
+                {
+                    item.Status = "进行中";
+                    s = $"-i \"{item.Path}\"";
+                    if (item.BitRate != "") s += $" -b:v {item.BitRate}";
+                    if (item.Resolution != "") s += $" -vf scale={item.Resolution.Replace("*", ":")}";
+                    if (item.BitRate == "" && item.Resolution == "") s += " -vcodec copy";
+                    s += $" -acodec copy \"{item.Directory}\\{item.TargetName}\"";
+                    Process process = new Process();
+                    ProcessStartInfo processStartInfo = new ProcessStartInfo("ffmpeg.exe", s);
+                    processStartInfo.UseShellExecute = false;
+                    process.StartInfo = processStartInfo;
+                    process.Start();
+                    process.WaitForExit();
+                    int r = process.ExitCode;
+                    if (r == 0) item.Status = "已完成"; else item.Status = "错误";
+                }
+            }).Start();
+        }
+
+        private void Button_Click_4(object sender, RoutedEventArgs e)
+        {
+            lMediaItem.Clear();
+        }
+    }
+}
---
+++ MediaTool/MediaItem.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace MediaTool
+{
+    class MediaItem : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public String Path { get; set; }
+        public String Directory { get; set; }
+        public String Name { get; set; }
+        public String Resolution { get; set; }
+        public String BitRate { get; set; }
+        public String TargetName { get; set; }
+        private String _status;
+        public String Status
+        {
+            get
+            {
+                return _status;
+            }
+            set
+            {
+                _status = value;
+                if (PropertyChanged != null) PropertyChanged(this, new PropertyChangedEventArgs("Status"));
+            }
+        }
+        private String _format;
+        public String Format
+        {
+            get
+            {
+                return _format;
+            }
+            set
+            {
+                _format = value;
+                TargetName = "output_" + Name.Substring(0, Name.LastIndexOf(".")) + "." + Format;
+                if (PropertyChanged != null) PropertyChanged(this, new PropertyChangedEventArgs("TargetName"));
+            }
+        }
+
+        public MediaItem(String path)
+        {
+            Path = path;
+            Directory = path.Substring(0, path.LastIndexOf("\\"));
+            Name = path.Substring(path.LastIndexOf("\\") + 1);
+            Resolution = "";
+            BitRate = "";
+            Format = path.Substring(path.LastIndexOf(".") + 1);
+            Status = "未开始";
+        }
+
+    }
+}
